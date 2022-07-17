832b5ddccb7154eff559aac576358e2d84cf1eb5
the-dmitry <dzmitrygaponenko@gmail.com>
2022-07-18 01:52:00 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrakets = bracketsConfig.map(arr => arr[0])
+    const closeBrakets = bracketsConfig.map(arr => arr[arr.length - 1])
+    let stack = []
+    for(let i = 0; i < str.length; i++) {
+        let topIndex = openBrakets.indexOf(stack[stack.length - 1])
+        const current = str[i]
+        if(closeBrakets.includes(current)) {
+            topIndex === closeBrakets.indexOf(current) ? stack.pop() : stack.push(current)   
+        } else {
+            stack.push(current)
+        } 
+    }
+    return stack.length === 0
 }
+
+
